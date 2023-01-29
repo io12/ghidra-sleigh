78a8a7cbc35dab2c93d1e4abd36766805f581fc0
Benjamin Levy <blevy@protonmail.com>
2023-01-29 14:42:11 -0500

    add x86 fuzzing

---
+++ arch/x86/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -0,0 +1,21 @@
+#![no_main]
+
+libfuzzer_sys::fuzz_target!(|data: &[u8]| {
+    let addr = 0x1234_u32; // TODO: fuzz this
+    let expected = {
+        let (len, insns) = sleigh::Decompiler::builder()
+            .x86(sleigh::X86Mode::Mode32)
+            .build()
+            .disassemble(data, addr.into());
+        if len <= data.len() {
+            insns
+                .get(0)
+                .map(|insn| format!("{} {}", insn.mnemonic, insn.body))
+        } else {
+            None
+        }
+    };
+    let actual = sleigh_x86::Instruction::disasm(data, addr, 0b00000000000000000000000001100000)
+        .map(|insn| insn.to_string());
+    assert_eq!(expected, actual, "input was {data:02X?}");
+});
